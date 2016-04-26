67b427ed4b2ce9206dae90f96fd286e757da2c35
jammingiraffe <maggieborkowski@gmail.com>
2016-04-26 17:34:30 -0400

    a bit of admin page customization; more django tutorial stuff

--- compsocsite/polls/models.py
+++ compsocsite/polls/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
@@ -10,6 +13,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+    	return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 @python_2_unicode_compatible
 class Choice(models.Model):

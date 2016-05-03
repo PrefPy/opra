bcde16e3023b31e2cbac1d4f6e0fa0182b912a14
pensive-tj <tkjohnston559@yahoo.com>
2016-05-03 16:45:22 -0400

    Added follow_up field, refined algorithm

--- compsocsite/polls/algorithms.py
+++ compsocsite/polls/algorithms.py
@@ -1,8 +1,19 @@
 from polls.models import Question, Item, Response, Student, Dictionary, KeyValuePair
+import operator
 
 def allocation_serial_dictatorship(the_question):
 	item_set = Item.objects.filter(question = the_question)
-	student_order = Response.objects.filter(question__question_text = the_question.question_text)
+	student_order = Response.objects.filter(question = the_question)
+	if the_question.follow_up != None:
+		responses = []
+		for r in student_order:
+			responses.append(r)
+
+		for r in responses:
+			temp = r.question.follow_up.response_set.filter(student = r.student)
+			r.timestamp = temp.timestamp
+		responses.sort(key = operator.attrgetter('timestamp'), reverse = True)
+		student_order = responses
 	items = []
 
 	for item in item_set:
--- compsocsite/polls/models.py
+++ compsocsite/polls/models.py
@@ -22,6 +22,7 @@ class Student(models.Model):
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    follow_up = models.OneToOneField('Question', on_delete=models.CASCADE, null = True, blank = True)
     def __str__(self):
         return self.question_text
     def was_published_recently(self):

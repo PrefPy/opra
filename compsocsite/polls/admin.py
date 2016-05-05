c81a00b9d04df0b0ed96b433f93a62ec9f69dbd9
jammingiraffe <maggieborkowski@gmail.com>
2016-05-04 23:11:42 -0400

    working on response view

--- compsocsite/polls/admin.py
+++ compsocsite/polls/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 
-from .models import Student, Question, Response, Item
+from .models import *
 
-# Register your models here.
+# https://gist.github.com/aaugustin/1388243
+class ReadOnlyModelAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin class that prevents modifications through the admin.
+    The changelist and the detail view work, but a 403 is returned
+    if one actually tries to edit an object.
+    Source: https://gist.github.com/aaugustin/1388243
+    """
+    actions = None
+
+    # We cannot call super().get_fields(request, obj) because that method calls
+    # get_readonly_fields(request, obj), causing infinite recursion. Ditto for
+    # super().get_form(request, obj). So we  assume the default ModelForm.
+    def get_readonly_fields(self, request, obj=None):
+        return self.fields or [f.name for f in self.model._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    # Allow viewing objects but not actually changing them.
+    # def has_change_permission(self, request, obj=None):
+    #     # return (request.method in ['GET', 'HEAD'] and
+    #     #         super().has_change_permission(request, obj))
+    #     return False
+
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+
 
 class ItemInline(admin.TabularInline):
     model = Item
@@ -26,5 +53,11 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('student_name', 'student_email')
     search_fields = ['student_name']
 
+class ResponseAdmin(ReadOnlyModelAdmin):
+    list_display = ('student', 'question', 'timestamp')
+    list_filter = ['student', 'question', 'timestamp']
+
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Student, StudentAdmin)
+admin.site.register(Response, ResponseAdmin)

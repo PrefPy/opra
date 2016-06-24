db21bde1f2b82478668e32d28825cf1cf8d7c7fd
Samuel Yuan <yuans@rpi.edu>
2016-06-23 21:11:23 -0400

    Fixed issue with lambda

--- compsocsite/groups/urls.py
+++ compsocsite/groups/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'groups'
 urlpatterns = [
-    url(r'^addgroup/$', views.addGroupView.as_view(), name='addgroup'),
+    url(r'^add/$', views.addGroupView.as_view(), name='addgroup'),
     url(r'^addgroupfunc/$', views.addgroup, name='addgroupfunc'),
-    url(r'^(?P<group_id>[0-9]+)/addmember/$', views.addmember, name='addmember'),
     url(r'^(?P<pk>[0-9]+)/members/$', views.MembersView.as_view(), name='members'),
+    url(r'^(?P<group_id>[0-9]+)/members/add/$', views.addmember, name='addmember'),    
     url(r'^(?P<question_id>[0-9]+)/addgroupvoters/$', views.addgroupvoters, name='addgroupvoters'),    
 ]

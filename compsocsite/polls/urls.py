3086bec297d97e635448f6cf405301a6dd62df1a
Samuel Yuan <yuans@rpi.edu>
2016-06-28 23:31:52 -0400

    Make the user commit to a polling algorithm

--- compsocsite/polls/urls.py
+++ compsocsite/polls/urls.py
@@ -25,11 +25,11 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/setting_step4/$', views.SettingStep4View.as_view(), name='setting_step4'),
     url(r'^(?P<pk>[0-9]+)/setting_step5/$', views.SettingStep5View.as_view(), name='setting_step5'),
     
-    
     url(r'^(?P<question_id>[0-9]+)/start/$', views.startPoll, name='start'),  
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     url(r'^(?P<question_id>[0-9]+)/stop/$', views.stopPoll, name='stop'),
-    url(r'^(?P<question_id>[0-9]+)/setview/$', views.setview, name='setview'),
+    url(r'^(?P<question_id>[0-9]+)/settings/algorithm$', views.setAlgorithm, name='setalgorithm'),    
+    url(r'^(?P<question_id>[0-9]+)/settings/visibility$', views.setVisibility, name='setview'),
     url(r'^(?P<pk>[0-9]+)/vote/results/$', views.VoteResultsView.as_view(), name='voteresults'),
     url(r'^(?P<pk>[0-9]+)/confirmation/$', views.ConfirmationView.as_view(), name='confirmation'),
     url(r'^(?P<pk>[0-9]+)/preferences/$', views.PreferenceView.as_view(), name='preferences'),

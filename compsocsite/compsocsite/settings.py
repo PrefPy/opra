51afcfc01fc8edfed636ad97559ac4ce918e2444
xialirong <xialirong@gmail.com>
2016-07-05 13:11:51 -0400

    correct static setting

--- compsocsite/compsocsite/settings.py
+++ compsocsite/compsocsite/settings.py
@@ -145,10 +145,10 @@ EMAIL_PORT = 587
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/items/static/items/'
+STATIC_URL = '/static/'
 LOGIN_URL = '/auth/login/'
 
 STATICFILES_DIRS = (
-    '/static/items/',
-    os.path.join(os.path.abspath(BASE_DIR), 'static', 'items'),
+    '/static/',
+    os.path.join(os.path.abspath(BASE_DIR), 'static'),
 )

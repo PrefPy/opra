8a176e7f05b4c21d89027d90ba38ded87130deb7
Samuel Yuan <yuans@rpi.edu>
2016-07-22 19:55:56 -0400

    Show submit button when the user is updating existing preferences

--- compsocsite/static/js/shared.js
+++ compsocsite/static/js/shared.js
@@ -59,9 +59,14 @@ function moveAll() {
 };
 
 $(function() {
+    //if the user updates existing preferences, the submit button should be shown
+    if ($('#right-sortable li').length == 0) {
+        enableSubmission();
+    }
+
+    
     $("list-group-item").sortable();
 	$("#left-sortable").sortable({
-       
         start: function(event, ui) {
         //     sortin=-1,
             item = ui.item;
@@ -99,9 +104,7 @@ $(function() {
 		//placeholder : "ui-sortable-placeholder",
 		containment : "document",
         connectWith : "#right-sortable",
-        
 	});
-
 
 	$("#left-sortable, #right-sortable").disableSelection();
 });

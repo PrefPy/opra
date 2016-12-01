bad25a871e94c5fb623fe542ac8d6dd07f6e0f74
TristanV12 <villamil101@gmail.com>
2016-12-01 00:07:55 -0500

    Disable mixtures for now

--- compsocsite/polls/views.py
+++ compsocsite/polls/views.py
@@ -720,9 +720,9 @@ def getVoteResults(latest_responses,candMap):
     scoreVectorList.append(MechanismSimplifiedBucklin().getCandScoresMap(pollProfile))
     scoreVectorList.append(MechanismCopeland(1).getCandScoresMap(pollProfile))
     scoreVectorList.append(MechanismMaximin().getCandScoresMap(pollProfile))
-    gmm = GMMMixPLAggregator(list(pollProfile.candMap.values()), use_matlab=False)
+    #gmm = GMMMixPLAggregator(list(pollProfile.candMap.values()), use_matlab=False)
     
-    return scoreVectorList, gmm.aggregate(pollProfile.getOrderVectors(), algorithm="top3_full", epsilon=.1, max_iters=10, approx_step=.1)
+    return scoreVectorList, [[.1] * 10]#gmm.aggregate(pollProfile.getOrderVectors(), algorithm="top3_full", epsilon=.1, max_iters=10, approx_step=.1)
     
 def calculatePreviousResults(request, question_id,candMap):
     question = get_object_or_404(Question, pk=question_id)

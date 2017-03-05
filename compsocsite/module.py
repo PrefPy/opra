77ecc5841521792496680e5cb4989dbc9fd2b3b4
TristanV12 <villamil101@gmail.com>
2017-03-04 23:11:24 -0500

    Create user villat2@rpi.edu instead of VILLAT2

--- compsocsite/module.py
+++ compsocsite/module.py
@@ -1,20 +1,15 @@
 from polls.models import *
 from appauth.models import *
+import xml.etree.ElementTree as ET
 
 def callbackfunction(tree):
+	print(ET.tostring(tree))
 	username = tree[0][0].text.lower() + "@rpi.edu"
-	print(username)
-	# user, user_created = User.objects.get_or_create(username=username)
-	# user.set_password(username)
-	# user.save()
-	# print(user)
-	# user = authenticate(username=username, password=username)
-	# print(user)
-	# try:
-	#  	var = user.userprofile
-	# except:
-	# 	profile = UserProfile(user = user, displayPref = 1)
-	# 	profile.email = username
-	# 	profile.save()
-	# 	user.is_active = True
-	# 	user.save()
+	tree[0][0].text = username
+	user, user_created = User.objects.get_or_create(username=username)
+	if(user_created):
+		profile = UserProfile(user = user, displayPref = 1)
+		profile.save()
+		user.email = username
+		user.is_active = True
+		user.save()

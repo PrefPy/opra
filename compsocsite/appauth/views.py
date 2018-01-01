6bdbee931c05778a4817e00727f2990607e24e0e
CanonYe <yec@rpi.edu>
2018-01-01 18:04:29 -0500

    bug fixed

--- compsocsite/appauth/views.py
+++ compsocsite/appauth/views.py
@@ -329,7 +329,7 @@ def createMturkUser(request):
         elif request.user.username != "":
             first_or_last = [42]
             list1 = [63,64]
-            list2 = list(range(43, 63))
+            list2 = list(range(53, 62))+list(range(66,75))
             random.shuffle(list2)
             flag = random.randrange(2)
             polls = []

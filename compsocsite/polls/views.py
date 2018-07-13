0bcd28707aa63d13d1538887782f34f50a8c7034
tomjmwang <tomjmwang@yahoo.com>
2018-07-13 09:38:12 -0400

    add problem

--- compsocsite/polls/views.py
+++ compsocsite/polls/views.py
@@ -2441,7 +2441,7 @@ def stopAttendance(request, pk):
 def attendanceSignIn(request, question_id):
     cur_poll = get_object_or_404(Question, pk=question_id)
     if request.user not in cur_poll.related_class.students.all():
-        cur_poll.related_class.add(request.user.id)
+        cur_poll.related_class.students.add(request.user.id)
     resp = Response(question=cur_poll, user=request.user, timestamp=timezone.now(),resp_str="[\"itemI'm here\"]")
     resp.save()
     return render(request, "success_join.html", {"poll_name":cur_poll.question_text})

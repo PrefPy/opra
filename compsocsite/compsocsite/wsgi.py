ea93ffe2a4fe4a94e9c93134935a7b3001d8042d
root <root@frog.cs.rpi.edu>
2019-05-02 15:54:16 -0400

    wsgi setting for server

--- compsocsite/compsocsite/wsgi.py
+++ compsocsite/compsocsite/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 #from whitenoise.django import DjangoWhiteNoise
+sys.path.append('/home/usr/local/lib/python3.6/dist-packages')
+sys.path.append('/home/OPRAH/opra')
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compsocsite.settings")
 
